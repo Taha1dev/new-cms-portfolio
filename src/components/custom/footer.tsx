6a6448d16c95d5418f7731ebc63dd71ba0fea71b
taha <taha@dev>
2025-08-28 05:43:25 +0300

    remove unused  data

--- src/components/custom/footer.tsx
+++ src/components/custom/footer.tsx
@@ -7,7 +7,7 @@ import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
-  Facebook,
+
   Linkedin,
   ArrowUp
 } from "lucide-react";
